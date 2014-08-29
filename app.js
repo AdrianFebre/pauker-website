45c96cdd8bd0d0896661c531dd13f1f4ca506baf
Adrian Febre <adrifeb@mac.com>
2014-08-29 13:08:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 var main = function() {
-    $('.dropdown-toggle').click(function() {
-        $('.dropdown-menu').toggle();
-    });
-
 
     $('.arrow-next').click(function() {
         var currentSlide = $('.active-slide');
@@ -12,10 +8,10 @@ var main = function() {
         var nextDot = currentDot.next();
 
         if(nextSlide.length === 0) {
-            nextSlide = $('.slide').first();
-            nextDot = $('.dot').first();
+          nextSlide = $('.slide').first();
+          nextDot = $('.dot').first();
         }
-
+    
         currentSlide.fadeOut(600).removeClass('active-slide');
         nextSlide.fadeIn(600).addClass('active-slide');
 
